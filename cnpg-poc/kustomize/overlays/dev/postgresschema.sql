b29f69c654a811e6cb15991c3041544070cb3371
Betraj J [CW] <betraj.j@skyhighsecurity.com>
2025-10-03 14:21:30 +0530

    applied changes

--- cnpg-poc/kustomize/overlays/dev/postgresschema.sql
+++ cnpg-poc/kustomize/overlays/dev/postgresschema.sql
@@ -13,7 +13,7 @@ INSERT INTO source (name) VALUES ('Other');
 
 
 CREATE TABLE urls(
-	url_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1),
+	url_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1),
 	url text NOT NULL,
 	domain_name_trellix varchar(256) NULL,
 	active_domain varchar(10) NOT NULL,
